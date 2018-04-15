c6d63379d57d002223ee1c506c58e44cd702a3a0
suyuanyuan21 <840424537@qq.com>
2018-04-15 17:15:39 +0800

    *添加在线播放和缓存到音乐文件夹后播放功能

--- NaiveMediaPlayer/NaiveMediaPlayer/MainPage.xaml.cs
+++ NaiveMediaPlayer/NaiveMediaPlayer/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -26,6 +30,7 @@ namespace NaiveMediaPlayer
         {
             this.InitializeComponent();
         }
+
         private async void FileButton_Click(object sender, RoutedEventArgs e)
         {
             var picker = new Windows.Storage.Pickers.FileOpenPicker();
@@ -43,13 +48,55 @@ namespace NaiveMediaPlayer
                 // Application now has read/write access to the picked file
                 FileName.Text = "正在播放 " + file.Name;
                 var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
                 naiveMediaplayer.SetSource(stream, file.ContentType);
+
                 naiveMediaplayer.Play();
             }
             else
             {
                 FileName.Text = "Operation cancelled.";
             }
+        }
+
+            private async void CacheButton_Click(object sender, RoutedEventArgs e)
+            {
+                try
+                {
+                    var httpClient = new HttpClient();
+                    Uri url = new Uri(uriString: URLBox.Text);
+                    var buffer = await httpClient.GetBufferAsync(url);
+                    FileName.Text = url.ToString();
+                    FileName.Text = FileName.Text.Substring(FileName.Text.LastIndexOf('/') + 1);
+
+                    if (buffer != null && buffer.Length > 0u)
+                    {
+                        var file = await KnownFolders.MusicLibrary.CreateFileAsync(FileName.Text, CreationCollisionOption.ReplaceExisting);
+                    using (var stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                    {
+                        await stream.WriteAsync(buffer);
+                        await stream.FlushAsync();
+                   
+                    }
+                    var streamfile = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+                    naiveMediaplayer.SetSource(streamfile, file.ContentType);
+                    naiveMediaplayer.Play();
+                        return;
+                    }
+                }
+                catch { }
+                return;
+
+            }
+            private void OnlineButton_Click(object sender, RoutedEventArgs e)
+        {
+            Uri url = new Uri(uriString: URLBox.Text);
+            naiveMediaplayer.Source = url;
+            FileName.Text = naiveMediaplayer.Source.ToString();
+            FileName.Text = FileName.Text.Substring(FileName.Text.LastIndexOf('/') + 1);
+            naiveMediaplayer.Play();
+
         }
     }
 }
